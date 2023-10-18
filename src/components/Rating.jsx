7f9b2d3c7e698c4b7b658f1aa67213e722c93fa9
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-18 18:34:27 -0400

    Finish adding elements to rating component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react"
+import starIcon from '../images/icon-star.svg';
 import '../styles/Rating.css';
 
 function Rating() {
+    const [rating, setRating] = useState();
+
+    function handleClick(number) {
+        setRating(number);
+    }
+
     return (
         <div className="rating-container">
             <div className="rating-selection-container">
-
+                <div className="star-container">
+                    <img src={starIcon} className="star-image" />
+                </div>
+                <div className="selection-title">How did we do?</div>
+                <div className="selection-subtitle">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</div>
+                <div className="number-row">
+                    <button className="number-button" onClick={() => handleClick(1)}>1</button>
+                    <button className="number-button" onClick={() => handleClick(2)}>2</button>
+                    <button className="number-button" onClick={() => handleClick(3)}>3</button>
+                    <button className="number-button" onClick={() => handleClick(4)}>4</button>
+                    <button className="number-button" onClick={() => handleClick(5)}>5</button>
+                </div>
+                <button className="submit-button">SUBMIT</button>
             </div>
         </div>
     )
