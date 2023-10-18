40ccc3e3d8f6f787f350bfbf2c3b76a31fcd5a6e
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-18 18:48:35 -0400

    Add conditional rendering for number-button-selected

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -18,11 +18,11 @@ function Rating() {
                 <div className="selection-title">How did we do?</div>
                 <div className="selection-subtitle">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</div>
                 <div className="number-row">
-                    <button className="number-button" onClick={() => handleClick(1)}>1</button>
-                    <button className="number-button" onClick={() => handleClick(2)}>2</button>
-                    <button className="number-button" onClick={() => handleClick(3)}>3</button>
-                    <button className="number-button" onClick={() => handleClick(4)}>4</button>
-                    <button className="number-button" onClick={() => handleClick(5)}>5</button>
+                    <button className={`number-button ${rating === 1 ? "number-button-selected" : ""}`} onClick={() => handleClick(1)}>1</button>
+                    <button className={`number-button ${rating === 2 ? "number-button-selected" : ""}`} onClick={() => handleClick(2)}>2</button>
+                    <button className={`number-button ${rating === 3 ? "number-button-selected" : ""}`} onClick={() => handleClick(3)}>3</button>
+                    <button className={`number-button ${rating === 4 ? "number-button-selected" : ""}`} onClick={() => handleClick(4)}>4</button>
+                    <button className={`number-button ${rating === 5 ? "number-button-selected" : ""}`} onClick={() => handleClick(5)}>5</button>
                 </div>
                 <button className="submit-button">SUBMIT</button>
             </div>
