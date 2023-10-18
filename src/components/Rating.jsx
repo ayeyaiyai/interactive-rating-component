c6b707780846a521c2dd000ac1764a72f075ec9f
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-18 16:53:25 -0400

    Add Rating component

---
+++ src/components/Rating.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import '../styles/Rating.css';
+
+function Rating() {
+    return (
+        <div className="rating-container">
+            <div className="rating-selection-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Rating;
